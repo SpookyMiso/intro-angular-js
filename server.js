7876fe1674218515675cd86dde501e7eb6373979
Jenn L <jenn.lazariuk@gmail.com>
2015-08-29 13:39:39 -1000

    imported angular library into html

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
+var path = require('path');
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+var server = app.listen(8080, function() {
+  console.log('Server running');
+});
