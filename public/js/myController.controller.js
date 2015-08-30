e3e0355e8b53e13546c188354f22d9a90ebc4471
Jenn L <jenn.lazariuk@gmail.com>
2015-08-29 16:12:10 -1000

    Created a injectable Service like a factory, except returns a service by invoking a constructor

--- public/js/myController.controller.js
+++ public/js/myController.controller.js
@@ -1,9 +1,13 @@
 (function () {
   angular
     .module('myApp')
-    .controller('myController', ['$scope', 'myAppTitle', 'CharacterVersionFactory', function ($scope, myAppTitle, CharacterVersionFactory) {
+    .controller('myController', ['$scope', 'myAppTitle', 'CharacterVersionFactory', 'BookService', myController]);
+
+    function myController ($scope, myAppTitle, CharacterVersionFactory, BookService) {
       $scope.myModel = "Katamari is life";
       $scope.title = myAppTitle;
       $scope.characters = CharacterVersionFactory;
-    }]);
+      $scope.books = BookService.getBooks();
+      $scope.book = BookService.getBook(1);
+    }
 })();
