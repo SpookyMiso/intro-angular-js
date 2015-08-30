61f83b5a0a24cd4c4b0e09f2565757ff6d75dd97
Jenn L <jenn.lazariuk@gmail.com>
2015-08-29 14:29:37 -1000

    moved the controller into a separate file

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,3 @@
-var app = angular.module('myApp', []);
-app.controller('myController', ['$scope', function ($scope) {
-  $scope.test = "Katamari is life";
-}]);
+(function () {
+  var app = angular.module('myApp', []);
+})();
---
+++ public/js/myController.controller.js
@@ -0,0 +1,7 @@
+(function () {
+  angular
+    .module('myApp')
+    .controller('myController', ['$scope', function ($scope) {
+      $scope.test = "Katamari is life";
+    }]);
+})();
