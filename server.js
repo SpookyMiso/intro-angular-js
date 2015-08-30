fe408960da1a97e1b45778fe1858565e7bae4f7c
Jenn L <jenn.lazariuk@gmail.com>
2015-08-29 19:37:19 -1000

    created a filter that matches user input to book titles

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api', function (req, res, next) {
   res.json([
-    { title: '007: Spectre' },
-    { title: 'Godzilla' },
-    { title: 'Pulp fiction' }
+    { title: '007: Spectre', year: 2015 },
+    { title: 'Godzilla', year: 1954},
+    { title: 'Pulp fiction', year: 1994 }
     ]);
 
 });
